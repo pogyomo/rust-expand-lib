4decb4ab1624d70c8eb8563f38dbe7ba24105e14
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2024-04-05 03:48:47 +0900

    refactor: use syn to parse code

--- src/main.rs
+++ src/main.rs
@@ -1,179 +1,233 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use clap::Parser;
+use quote::ToTokens;
 use std::{
-    ffi::OsString,
     fmt::Write as _,
-    fs::{canonicalize, File},
-    io::{BufRead, BufReader, BufWriter, Write},
-    path::{Component, PathBuf},
+    fs::{read_to_string, File},
+    io::{stdout, Write},
+    path::PathBuf,
+    process::Command,
 };
+use syn::{parse_file, Attribute, Expr, Ident, Item, Lit, Visibility};
+use tempfile::NamedTempFile;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Path to library crate to expand
     crate_path: String,
 
+    /// Format expanded code. rustfmt must be executable
+    #[arg(short, long)]
+    format: bool,
+
+    /// Remove all modules and functions which #[cfg(test)] is attached
+    #[arg(long)]
+    remove_test: bool,
+
+    /// Remove doc comments
+    #[arg(long)]
+    remove_doc_comment: bool,
+
+    /// Path to file to paste expanded code
     #[arg(short, long, value_name = "INPUT")]
     input: Option<PathBuf>,
 
+    /// Path to file to paste generated code
     #[arg(short, long, value_name = "OUTPUT")]
     output: Option<PathBuf>,
 }
 
-// TODO: This function is too long and must be separated into some functions.
-fn expand(root: &mut PathBuf, reading_mod_name: OsString, reading_lib: bool) -> Result<String> {
-    if !root.is_file() {
-        bail!("{} is a directory", root.display());
-    }
-    let file = File::open(&root).with_context(|| format!("failed to open {}", root.display()))?;
-    let lines = BufReader::new(file).lines();
+/// Expand modules in `path`/`name.rs` and return it as string.
+fn expand(
+    name: &str,
+    path: &mut PathBuf,
+    expanding_lib: bool,
+    remove_test: bool,
+    remove_doc_comment: bool,
+) -> Result<String> {
+    let content = {
+        path.push(name);
+        path.set_extension("rs");
+        let content = read_to_string(&path)
+            .context(format!("failed to read content of {}", path.display()))?;
+        path.pop();
+        content
+    };
+    let ast =
+        parse_file(&content).context(format!("failed to parse content of {}", path.display()))?;
     let mut res = String::new();
-    for line in lines {
-        let line = line.with_context(|| format!("failed to create line from file"))?;
-        let mut tokens = line.split_whitespace();
-        let is_pub = match tokens.clone().next() {
-            Some("pub") => {
-                tokens.next();
-                true
-            }
-            _ => false,
-        };
-        let head = tokens.next();
-        if matches!(head, Some("//") | Some("///")) {
-            continue;
-        } else if head != Some("mod") {
-            writeln!(res, "{line}").with_context(|| format!("failed to write line"))?;
-            continue;
-        }
-        let Some(mod_name) = tokens.next() else {
-            bail!("No module name exist after `mod`");
-        };
-        if mod_name.chars().last().unwrap() == ';' {
-            // extract module name
-            let mod_name = &mod_name[0..mod_name.len() - 1];
-
-            let (is_mod_file, is_lib_file) = if reading_lib {
-                root.pop(); // just remove lib.rs
-                (false, true)
-            } else {
-                // we are reading .../mod_name.rs or .../mod_name/mod.rs
-                // in both cases, next root is .../mod_name, so
-                // * root == .../mod_name.rs => replace mod_name.rs to mod_name
-                // * root == .../mod_name/mod.rs => just remove mod.rs
-                let last = root.components().last().unwrap();
-                match last {
-                    Component::Normal(last) => {
-                        if last == "mod.rs" {
-                            root.pop();
-                            (true, false)
-                        } else {
-                            root.pop();
-                            root.push(&reading_mod_name);
-                            (false, false)
-                        }
-                    }
-                    _ => bail!(
-                        "Unexpected component was taken from last of {}",
-                        root.display()
-                    ),
-                }
-            };
-
-            // try open root/mod_name.rs and expand it
-            root.push(format!("{mod_name}.rs"));
-            let childs = if root.exists() {
-                expand(root, OsString::from(mod_name), false)?
-            } else {
-                // try open root/mod_name/mod.rs and expand it
-                root.pop();
-                root.push(mod_name);
-                if root.exists() {
-                    expand(root, OsString::from(mod_name), false)?
-                } else {
-                    let a = root.clone();
-                    root.pop();
-                    root.push(format!("{mod_name}.rs"));
-                    let b = root;
-                    bail!(
-                        "Failed to open module {mod_name}. Neither of {} nor {} is exist",
-                        a.display(),
-                        b.display()
-                    );
-                }
-            };
-            if is_pub {
-                writeln!(res, "pub mod {mod_name} {{")?;
-            } else {
-                writeln!(res, "mod {mod_name} {{")?;
-            }
-            for line in childs.lines() {
-                writeln!(res, "    {line}")?;
-            }
-            writeln!(res, "}}")?;
-            root.pop();
-
-            if is_mod_file {
-                root.push("mod.rs");
-            } else if is_lib_file {
-                root.push("lib.rs");
-            } else {
-                root.pop();
-                root.push(format!("{}.rs", reading_mod_name.to_str().unwrap()));
-            }
-        } else {
-            writeln!(res, "{line}")?;
-            continue;
-        }
+    if let Some(shebang) = ast.shebang {
+        writeln!(res, "{shebang}")?;
+    }
+    for attr in ast.attrs {
+        writeln!(res, "{}", attr_to_string(remove_doc_comment, attr)?)?;
+    }
+    for item in ast.items {
+        let string = item_to_string(
+            name,
+            path,
+            expanding_lib,
+            remove_test,
+            remove_doc_comment,
+            item,
+        )?;
+        writeln!(res, "{}", string)?;
     }
     Ok(res)
 }
 
+/// Convert given attribute into string.
+fn attr_to_string(remove_doc_comment: bool, attr: Attribute) -> Result<String> {
+    if attr.path().is_ident("doc") {
+        if remove_doc_comment {
+            Ok(String::new())
+        } else {
+            let expr = attr
+                .parse_args::<Expr>()
+                .context("failed to parse argument of doc comment")?;
+            let Expr::Lit(lit) = expr else {
+                return Err(anyhow!("failed to parse argument of doc comment")
+                    .context("unexpected argument type of doc comment"));
+            };
+            let Lit::Str(str) = lit.lit else {
+                return Err(anyhow!("failed to parse argument of doc comment")
+                    .context("unexpected argument type of doc comment"));
+            };
+            Ok(str.value())
+        }
+    } else {
+        Ok(attr.to_token_stream().to_string())
+    }
+}
+
+/// Convert given item into string.
+fn item_to_string(
+    name: &str,
+    path: &mut PathBuf,
+    expanding_lib: bool,
+    remove_test: bool,
+    remove_doc_comment: bool,
+    item: Item,
+) -> Result<String> {
+    let Item::Mod(module) = item else {
+        // TODO: remove #[doc = ...] from item when remove_doc_comment is true
+        return Ok(item.to_token_stream().to_string());
+    };
+
+    // If one of attributes is #[cfg(test)], ignore the module and just return empty vector.
+    // Then, if the module is mod name { .. }, just write the content and return it.
+    let mut res = String::new();
+    for attr in module.attrs {
+        if !remove_test && !remove_doc_comment {
+            writeln!(res, "{}", attr_to_string(remove_doc_comment, attr)?)?;
+            continue;
+        }
+        if attr.path().is_ident("cfg") && remove_test {
+            let Ok(args) = attr.parse_args::<Ident>() else {
+                writeln!(res, "{}", attr_to_string(remove_doc_comment, attr)?)?;
+                continue;
+            };
+            if args.to_string() == "test" {
+                return Ok(String::new());
+            } else {
+                writeln!(res, "{}", attr_to_string(remove_doc_comment, attr)?)?;
+            }
+        } else if attr.path().is_ident("doc") && remove_doc_comment {
+            continue;
+        } else {
+            writeln!(res, "{}", attr_to_string(remove_doc_comment, attr)?)?;
+        }
+    }
+    if let Some(mod_content) = module.content {
+        for item in mod_content.1 {
+            let string = item_to_string(name, path, false, remove_test, remove_doc_comment, item)?;
+            writeln!(res, "{}", string)?;
+        }
+        return Ok(res);
+    }
+
+    // TODO: We don't expect mod_name/mod.rs exist instead of mod_name.rs exist.
+    let mod_name = module.ident.to_string();
+    if !expanding_lib {
+        path.push(name);
+    }
+    let mod_content = expand(&mod_name, path, false, remove_test, remove_doc_comment)?;
+    if !expanding_lib {
+        path.pop();
+    }
+
+    // Write expanded child modules.
+    match module.vis {
+        Visibility::Inherited => write!(res, "mod {mod_name} {{")?,
+        Visibility::Public(_) => write!(res, "pub mod {mod_name} {{")?,
+        _ => bail!("pub(..) is not supported"),
+    }
+    writeln!(res, "{mod_content} }}")?;
+    Ok(res)
+}
+
+/// Format given rust source code using rustfmt.
+fn format_code(code: String) -> Result<String> {
+    let mut file = NamedTempFile::new()?;
+    file.write_all(code.as_bytes())?;
+    Command::new("rustfmt").arg(file.path()).output()?;
+    Ok(read_to_string(file.path())?)
+}
+
+/// Convert crate name that can be used in source code.
+fn normalize_crate_name(name: &str) -> String {
+    name.chars()
+        .map(|c| if c == '-' { '_' } else { c })
+        .collect()
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
-    let (mut path_to_src_root, crate_name) = match canonicalize(&args.crate_path) {
-        Ok(p) => {
-            let maybe_crate_name = p
-                .components()
-                .last()
-                .with_context(|| format!("No component found from given path"))?;
-            match maybe_crate_name {
-                Component::Normal(crate_name) => {
-                    let crate_name = crate_name.to_owned();
-                    (p, crate_name)
-                }
-                _ => bail!("todo"),
-            }
-        }
-        Err(e) => {
-            bail!("Failed to get absolute path of {}: {e}", args.crate_path);
-        }
-    };
-    path_to_src_root.push("src");
-    path_to_src_root.push("lib.rs");
-    let expanded = expand(&mut path_to_src_root, OsString::from(""), true)?;
-    let mut output: BufWriter<Box<dyn Write>> = match args.output {
-        Some(output) => BufWriter::new(Box::new(File::create(output)?)),
-        None => BufWriter::new(Box::new(std::io::stdout().lock())),
-    };
-    if let Some(input) = args.input {
-        for line in BufReader::new(File::open(input)?).lines() {
-            writeln!(output, "{}", line?)?;
-        }
-        writeln!(output)?;
-    }
-    writeln!(
-        output,
-        "pub mod {} {{",
-        crate_name
-            .to_str()
-            .unwrap()
-            .chars()
-            .map(|c| if c == '-' { '_' } else { c })
-            .collect::<String>()
+
+    let mut src_path = PathBuf::from(&args.crate_path);
+    src_path.push("src");
+    let expanded = expand(
+        "lib",
+        &mut src_path,
+        true,
+        args.remove_test,
+        args.remove_doc_comment,
     )?;
-    for line in expanded.lines() {
-        writeln!(output, "    {}", line)?;
+
+    let code = {
+        let mut code = String::new();
+        if let Some(path) = args.input {
+            writeln!(code, "{}", read_to_string(path)?)?;
+        }
+        let path = PathBuf::from(args.crate_path);
+        let crate_name = path
+            .file_name()
+            .context("failed to extract crate name from path")?
+            .to_str()
+            .context("invalid utf-8 encoded crate name")?;
+        writeln!(
+            code,
+            "pub mod {} {{ {expanded} }}",
+            normalize_crate_name(crate_name)
+        )?;
+        if args.format {
+            format_code(code).context("failed to format generated code")?
+        } else {
+            code
+        }
+    };
+
+    if let Some(path) = args.output {
+        File::create(path)
+            .context("failed to create output file")?
+            .write_all(code.as_bytes())
+            .context("failed to write generated code to output file")?;
+    } else {
+        stdout()
+            .lock()
+            .write_all(code.as_bytes())
+            .context("failed to write generated code to stdout")?;
     }
-    writeln!(output, "}}")?;
     Ok(())
 }
